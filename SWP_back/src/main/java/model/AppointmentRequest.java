3c5ff19159e80c4c080e68a984185c174b4c797f
nguyenhuy238 <he186319nguyenquochuy@gmail.com>
2025-06-17 20:49:49 +0700

    Update : AppointmentServlet, AppointmentDAO, AppointmentRequest, addAppointment

---
+++ SWP_back/src/main/java/model/AppointmentRequest.java
@@ -0,0 +1,21 @@
+package model;
+
+public class AppointmentRequest {
+    private String doctorId;
+    private String date;
+    private String time;
+    private String note;
+    private String department;
+
+    // Getters and setters
+    public String getDoctorId() { return doctorId; }
+    public void setDoctorId(String doctorId) { this.doctorId = doctorId; }
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+    public String getTime() { return time; }
+    public void setTime(String time) { this.time = time; }
+    public String getNote() { return note; }
+    public void setNote(String note) { this.note = note; }
+    public String getDepartment() { return department; }
+    public void setDepartment(String department) { this.department = department; }
+}
